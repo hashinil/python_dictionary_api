218e0f9ea835f051c2fe8d72a1acbea1c75dbf62
Hashini <hashini.liyanage@gmail.com>
2023-08-01 23:41:42 +0200

    read csv and load data

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template
+import pandas as pd
 
-app = Flask("Website")
+app = Flask("__name__")
 
-@app.route("/home")
+filename = "data/dictionary.csv"
+df = pd.read_csv(filename)
+
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
+    return render_template("home.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
 
-app.run(debug=True)
+@app.route("/api/v1/<word>/")
+def details(word):
+    definition = df.loc[df['word'] == word]['definition'].squeeze()
+    #word = word.capitalize()
+    result = {"definition": definition, "word": word}
+    return result
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
